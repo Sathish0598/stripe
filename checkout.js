5e79e94091e899b89ebe94b662ec87713d38fbff
Sathish0598 <mca.skp.05@gmail.com>
2023-02-08 23:37:11 +0530

    born

---
+++ checkout.js
@@ -0,0 +1,19 @@
+import { loadStripe } from "@stripe/stripe-js";
+
+export async function checkout({lineItem}){
+    let stripePromise = null
+    const getStripe = () =>{
+        if(!stripePromise){
+            stripePromise = loadStripe(process.env.NEXT_PUBLIC_API_KEY)
+        }
+        return stripePromise
+    }
+    const stripe =await getStripe()
+
+    await stripe.redirectToCheckout({
+        mode:'payment',
+        lineItem,
+        successUrl: `${window.location.origin}?session_id={CHECKOUT_SESSION_ID}`,
+        cancelUrl: window.location.origin
+    })
+}
